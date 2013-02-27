9c01af06d43b6d81eff1081399b4111b99adb13a
Joe Hitchens <joe@sleepless.com>
2013-02-27 15:01:38 -0800

    foo blah

---
+++ nothing/nothing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nothing
+//
+//  Created by joseph hitchens on 2/27/13.
+//  Copyright (c) 2013 Sleepless Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nothing/nothing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  nothing
+//
+//  Created by joseph hitchens on 2/27/13.
+//  Copyright (c) 2013 Sleepless Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}

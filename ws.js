9d9e159ccb1dfc8fb5d3beb7731d72a514530844
Joe Hitchens <joe@sleepless.com>
2017-09-16 19:41:15 -0700

    -

---
+++ ws.js
@@ -0,0 +1,21 @@
+
+// Copyright 2017 Sleepless Software Inc.  All Rights Reserved 
+
+
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var sleepless = require("sleepless");
+
+
+
+function WebServer() {
+	var self = this;
+
+}
+
+
+
+module.exports = Server;
+
+

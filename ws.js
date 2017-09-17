dee5ff4d041b0e508d488898d51ba7a83d1e375d
Joe Hitchens <joe@sleepless.com>
2017-09-16 20:53:57 -0700

    -

--- ws.js
+++ ws.js
@@ -1,21 +1,70 @@
 
 // Copyright 2017 Sleepless Software Inc.  All Rights Reserved 
+"use strict";
 
 
-var fs = require("fs");
-var http = require("http");
-var https = require("https");
-var sleepless = require("sleepless");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
+const parseUrl = require("parseurl");
+const send = require("send");
+
+const sleepless = require("sleepless");
 
 
-
-function WebServer() {
-	var self = this;
-
+function r404(rsp) {
+	rsp.StatusCode = 404;
+	rsp.end("404: NOT FOUND\n");
 }
 
+function request(req, rsp) {
+
+	log(req.method + " " + req.url);
+
+	var pu = parseUrl(req);
+	var path = pu.pathname;
+
+	function error(err) {
+		// send wasn't able to deliver a static file for some reason
+
+		// try to deal with dynamic content.
+		var mod_path = path + "/index.js";
+		fs.stat(mod_path, (err, st) => {
+			if(err) {
+				log("stat error: "+err);
+				r404(rsp);
+			}
+			else
+			if(!st.isFile()) {
+				log("not a file: "+mod_path);
+				r404(rsp);
+			}
+			else {
+				try {
+					var mod = require(mod_path);
+					log("calling module: "+mod_path);
+					mod(req, rsp);
+				}
+				catch(e) {
+					log("can't load module: "+e);
+					r404(rsp);
+				}
+			}
+		});
+	};
+
+	send(req, path, { root: "site" }).on("error", error).pipe(rsp);
+
+};
 
 
-module.exports = Server;
+var server = http.createServer();
+
+server.on("request", request);
+
+server.listen(12345, () => { log("listening"); });
+
+
 
 

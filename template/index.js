f7702df069747185a56d364259118b06ca393951
Zafuzi <zacharyfoutz@gmail.com>
2021-04-08 16:58:20 -0700

    fixed file copy and watch script, rpc now cold reloads static files

--- template/index.js
+++ template/index.js
@@ -1,15 +1,16 @@
+const L = require( "log5" ).mkLog( "rpc: " )( 5 );
 let rpc = function( req, res, cb ) {
-	console.log("rpc call made");
+	L.V( req.url );
 
 	// do rpc call 
 	let act = null;
 	if( act ) {
-
-		console.log( act );
+		L.I( act );
 		cb();
 	}
 
 	// drop back to static, or handle it myself
+	req.url = `static/${req.url}`;
 	cb( req, res );
 }
 

4fd2d008a85fdc41dc348998da9175a7a31aed2e
Zafuzi <zacharyfoutz@gmail.com>
2021-04-08 20:00:29 -0700

    * serve static files form api folder?

--- template/index.js
+++ template/index.js
@@ -5,11 +5,18 @@ const L = require( "log5" ).mkLog( "router: " )( 5 );
 let router = function( req, res, cb ) {
 	if( req?.url != "/reload.js" ) { L.V( req.url ); }
 
+	// static serve api files?
+	if( req.url.startsWith("/api/") ) {
+		req.url = `static/${req.url}`;
+		cb( req, res );
+		return;
+	}
+
 	if ( req.url.startsWith("/api") ) {
 		let mod = mod_load( __dirname + "/api" );
 		if( mod ) {
 			L.D("api module loaded ");
-		   	mod( req, res, (r1, r2) => { cb( r1, r2 ); });
+		   	mod( req, res, () => { cb( req, res ); });
 		} else {
 			L.I("mod not found");
 		}
